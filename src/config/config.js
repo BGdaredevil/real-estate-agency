e244dafba0632f6a8034d0c6e44c2b0eaeb08cd2
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-10-17 11:12:30 +0300

    minor

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || "27017",
-    appPort: process.env.APP_PORT || "3030",
+    appPort: process.env.APP_PORT || "5000",
     table: process.env.DB_TABLE || "Housing",
     dBaseUrl: `mongodb://${process.env.DB_HOST || "localhost"}:${process.env.DB_PORT || "27017"}/${
       process.env.DB_TABLE || "Housing"
